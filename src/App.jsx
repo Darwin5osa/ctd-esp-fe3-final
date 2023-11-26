00fbb07580d797d0a7791d09fedfac8feac6c37e
darwi <darwinsosa2004@gmail.com>
2023-11-26 02:11:28 -0300

    Se agregaron ruteo con nav bar

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,22 @@
-
+import { Route, Routes } from 'react-router-dom';
 import Footer from "./Components/Footer";
 import Navbar from "./Components/Navbar";
-import Home from "./Routes/Home";
-//llamado al home para probarlo
+import Favs from './Routes/Favs';
+import Contact from './Routes/Contact';
+import Home from './Routes/Home';
+import Detail from './Routes/Detail';
 
 function App() {
   return (
       <div className="App">
           <Navbar/>
-          <Home/>
+          <Routes>
+            <Route path='/' element={<Home/>}/>
+            <Route path='/contact' element={<Contact/>}/>
+            <Route path='/favs' element={<Favs/>}/>
+            <Route path='/detail/:id' element={<Detail />} />
+            {/*<Route path='*' element={<PageNotFound/>}/>*/}
+          </Routes>
           <Footer/>
       </div>
   );
--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,13 +1,15 @@
 import React from 'react'
+import { Link, useNavigate } from 'react-router-dom'
 
 //Este componente debera ser estilado como "dark" o "light" dependiendo del theme del Context
 
 const Navbar = () => {
-
+  
   return (
     <nav>
-      {/* Aqui deberan agregar los liks correspondientes a las rutas definidas */}
-      {/* Deberan implementar ademas la logica para cambiar de Theme con el button */}
+      <Link to='/'><h4>Home</h4></Link>
+      <Link to='/contacto'><h4>Contacto</h4></Link>
+      <Link to='/favs'><h4>Favs</h4></Link>
       <button>Change theme</button>
     </nav>
   )
