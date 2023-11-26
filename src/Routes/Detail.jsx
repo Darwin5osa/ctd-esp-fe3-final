aad3bf465216093875193f52854cbba91920b49a
María Fernanda Ardila <mfardilam@MacBook-Air-de-Maria.local>
2023-11-26 15:26:43 -0500

    Esqueleto de Detail, falta ajustarlo con el context

--- src/Routes/Detail.jsx
+++ src/Routes/Detail.jsx
@@ -1,15 +1,42 @@
-import React from 'react'
+import axios from 'axios'
+import React, { useEffect, useState } from 'react'
+import { useParams } from 'react-router-dom'
 
 
 //Este componente debera ser estilado como "dark" o "light" dependiendo del theme del Context
 
 const Detail = () => {
+
+  //temporal: 
+  const [dentist, setDentist] = useState({})
  
   // Consumiendo el parametro dinamico de la URL deberan hacer un fetch a un user en especifico
+  const params = useParams()
+  const url = `https://jsonplaceholder.typicode.com/users/${params.id}`
+
+  useEffect(() => {
+    axios(url)
+    .then(response => setDentist(response.data))
+  })
 
   return (
     <>
-      <h1>Detail Dentist id </h1>
+      <h1>Detail Dentist - ID: {dentist.id}</h1>
+      <table>
+        <tr>
+          <th>Name</th>
+          <th>Email</th>
+          <th>Telephone</th>
+          <th>Website</th>
+        </tr>
+        <tr>
+          <th>{dentist.name}</th>
+          <th>{dentist.email}</th>
+          <th>{dentist.phone}</th>
+          <th>{dentist.website}</th>
+        </tr>
+      </table>
+
       {/* aqui deberan renderizar la informacion en detalle de un user en especifico */}
       {/* Deberan mostrar el name - email - phone - website por cada user en especifico */}
     </>
